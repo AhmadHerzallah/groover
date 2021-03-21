3c13d7b0edbc9c43b5707a1751dd76aaf7d13c13
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-03-21 12:57:40 +0200

    init

---
+++ src/style/global.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    ${'' /* align-items: center; */}
+    background: ${({ theme }) => theme.body};
+    color: #FAFAFA;
+    ${'' /* display: flex; */}
+    ${'' /* flex-direction: column; */}
+    ${'' /* justify-content: center; */}
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    ${
+      '' /* font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; */
+    }
+    font-family: 'TT Norms Pro';
+    transition: all 0.25s linear;
+  }
+
+  ::selection {
+    background-color: #7f5af0;
+    font-weight: 700 !important;
+
+    color: black;
+  }
+  .navbar {
+    background-color: ${({ theme }) => theme.nav} !important;
+  }
+  .navbar-brand {
+    color: ${({ theme }) => theme.text} !important;
+  }
+  .navbar-nav a{
+    color: ${({ theme }) => theme.text} !important;
+  }
+  .navbar-brand a {
+    color: ${({ theme }) => theme.text} !important;
+    text-decoration: none;
+}
+  .react-switch {
+    margin-top: 18px;
+    margin-right: 20px;
+    margin-left: 10px;
+  }
+
+  ${
+    '' /* .react-switch-bg > div:first-of-type {
+    margin-left: 7px;
+    padding-top: 1px;
+  }
+  .react-switch-bg > div:last-of-type {
+    margin-right: 7px;
+    padding-top: 1.5px;
+  } */
+  }
+
+  `;
---
+++ src/style/theme.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  body: '#E2E2E2',
+  text: '#363537',
+  toggleBorder: '#FFF',
+  gradient: 'linear-gradient(#39598A, #79D7ED)',
+  switchbtn: '#363537',
+  nav: '#fffffe',
+  burger: '#363537',
+};
+
+export const darkTheme = {
+  body: '#16161a',
+  text: '#FAFAFA',
+  toggleBorder: '#6B8096',
+  gradient: 'linear-gradient(#091236, #1E215D)',
+  switchbtn: '#E2E2E2',
+  nav: '#16161a',
+  burger: '#FAFAFA',
+};
