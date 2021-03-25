2aed3b5a3cd7791d5c21096e73e9ec3e08456c03
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-03-25 22:39:06 +0200

    remove file

--- src/Components/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-if (localStorage.getItem("ArtistsHistory") !== null) {
-    let artistlist = localStorage.getItem("ArtistsHistory");
-    artistlist = JSON.parse(artistlist);
-    artistlist = JSON.stringify(artistlist);
-    local
