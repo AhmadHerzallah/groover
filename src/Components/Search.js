4c5e551cdbcf1b7a882804120690d083e801f5bc
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-03-26 22:05:46 +0300

    .

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-target-blank */
-import React, { useEffect, useState } from "react";
-import Style from "../style/search.module.css";
-import Form from "react-bootstrap/Form";
-import Switch from "react-switch";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import Jdenticon from "react-jdenticon";
+import React, { useEffect, useState } from 'react';
+import Style from '../style/search.module.css';
+import Form from 'react-bootstrap/Form';
+import Switch from 'react-switch';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Jdenticon from 'react-jdenticon';
 
 const Search = () => {
   const [switcher, setSwitcher] = useState(true);
-  const [query, setQuery] = useState("JuiceWRLD");
+  const [query, setQuery] = useState('JuiceWRLD');
   const [flag, setFlag] = useState(false);
   const [artistData, setArtistData] = useState({});
   const [trackData, setTrackData] = useState({});
@@ -22,22 +22,23 @@ const Search = () => {
   const handleSwitchChange = () => {
     setSwitcher(!switcher);
   };
-
   const token =
-    "BQAobH14qd0XbmyHA3FrZ9roj866WwJ-YUK5JE9RCXrJeccqGF5Di5Fguht-bopv_N8Cm3CbnZuB_j2WxV4PHZKa2aheiytSByFyuxdBNG-izDRPAeApjwpfQfCwHGmpg7cHEUPUjmu0dveqMyv7VE0NYOGhEUIjtjPBWavDOnNqMKNjtA";
+    'BQAj8PioTHGpDVd8TEoqL_XI47SNtfuNpVrMIdXkLUchb5ithPJgEAZZZHaE1D_52aRBFzX2diDk4BfZ0hdivYBFq8ybjEK50exd-v3nH8fXgqgrhH8am3XrxsGLnij20HDcxp92b87DPM-4pNNmA1PJbaJCNiS1U2paUkOfKzndHbzdRw';
 
+  // the fetch is not working because u r a woman
   const getDataArtist = async () => {
     const res = await fetch(
-      `https://api.spotify.com/v1/search?q=${query}&type=artist&limit=10&access_token=${token}`
+      `https://api.spotify.com/v1/search?q=${query}&type=artist&limit=10&access_token=${token}`,
     );
     const data = await res.json();
     // console.log('searching..');
     setArtistData(data);
   };
 
+  // the fetch is not working because u r a woman
   const getDataTrack = async () => {
     const res = await fetch(
-      `https://api.spotify.com/v1/search?q=${query}&type=track&limit=10&access_token=${token}`
+      `https://api.spotify.com/v1/search?q=${query}&type=track&limit=10&access_token=${token}`,
     );
     const data = await res.json();
     // console.log('searching..');
@@ -46,31 +47,31 @@ const Search = () => {
   let artistlist = [];
   let tracklist = [];
   const changeAhistory = () => {
-    if (localStorage.getItem("AH") != null) {
-      if (Array.isArray(JSON.parse(localStorage.getItem("AH")))) {
-        artistlist = JSON.parse([localStorage.getItem("AH")]);
+    if (localStorage.getItem('AH') != null) {
+      if (Array.isArray(JSON.parse(localStorage.getItem('AH')))) {
+        artistlist = JSON.parse([localStorage.getItem('AH')]);
         artistlist.push(query);
       } else {
-        artistlist = [JSON.parse(localStorage.getItem("AH"))];
+        artistlist = [JSON.parse(localStorage.getItem('AH'))];
         artistlist.push(query);
       }
-      localStorage.setItem("AH", JSON.stringify(artistlist));
+      localStorage.setItem('AH', JSON.stringify(artistlist));
     } else {
-      localStorage.setItem("AH", JSON.stringify(query));
+      localStorage.setItem('AH', JSON.stringify(query));
     }
   };
   const changeThistory = () => {
-    if (localStorage.getItem("TH") != null) {
-      if (Array.isArray(JSON.parse(localStorage.getItem("TH")))) {
-        tracklist = JSON.parse([localStorage.getItem("TH")]);
+    if (localStorage.getItem('TH') != null) {
+      if (Array.isArray(JSON.parse(localStorage.getItem('TH')))) {
+        tracklist = JSON.parse([localStorage.getItem('TH')]);
         tracklist.push(query);
       } else {
-        tracklist = [JSON.parse(localStorage.getItem("TH"))];
+        tracklist = [JSON.parse(localStorage.getItem('TH'))];
         tracklist.push(query);
       }
-      localStorage.setItem("TH", JSON.stringify(tracklist));
+      localStorage.setItem('TH', JSON.stringify(tracklist));
     } else {
-      localStorage.setItem("TH", JSON.stringify(query));
+      localStorage.setItem('TH', JSON.stringify(query));
     }
   };
   const handleSubmit = (e) => {
@@ -93,9 +94,9 @@ const Search = () => {
             <Col md={3} sm={12}>
               <div className={Style.search__swtiches}>
                 <p className={Style.search__state}>
-                  Type:{" "}
+                  Type:{' '}
                   <span className={Style.search__current}>
-                    {switcher ? "Artist" : "Track"}
+                    {switcher ? 'Artist' : 'Track'}
                   </span>
                 </p>
                 <Switch
@@ -143,17 +144,17 @@ const Search = () => {
                   <li>
                     {res.genres.map((elem) => (
                       <span>
-                        {elem}{" "}
+                        {elem}{' '}
                         {res.genres.length === res.genres.indexOf(elem) + 1
                           ? null
-                          : ", "}
+                          : ', '}
                       </span>
                     ))}
                   </li>
                   <li>
                     <span className={Style.search__results__spotify}>
                       Spotify:
-                    </span>{" "}
+                    </span>{' '}
                     <a href={res.external_urls.spotify} target="_blank">
                       {res.name}
                     </a>
@@ -189,24 +190,24 @@ const Search = () => {
                       <li className={Style.search__results__artists}>
                         {res.artists.map((elem) => (
                           <span>
-                            {elem.name}{" "}
+                            {elem.name}{' '}
                             {res.artists.length ===
                             res.artists.indexOf(elem) + 1
                               ? null
-                              : ", "}
+                              : ', '}
                           </span>
                         ))}
                       </li>
                       <li className={Style.search__results__spotify__player}>
-                        {console.log(res.uri.split(":"))}
+                        {console.log(res.uri.split(':'))}
                         <iframe
                           src={`https://open.spotify.com/embed/${
-                            res.uri.split(":")[1]
-                          }/${res.uri.split(":")[2]}`}
+                            res.uri.split(':')[1]
+                          }/${res.uri.split(':')[2]}`}
                           width="240"
                           title="Spotify"
                           height="80"
-                          style={{ borderRadius: "5px" }}
+                          style={{ borderRadius: '5px' }}
                           frameborder="0"
                           allowtransparency="true"
                           allow="encrypted-media"
