410ef3ba489a8c9c5bd338b0990af83bbefd5922
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-04-12 10:59:32 +0300

    add: mood picker

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Style from '../style/home.module.css';
 const Home = ({ mute, handleMuteChange }) => {
   console.log(mute);
+
+  const allMoods = [
+    {
+      moods: ['Hyped'],
+      artist: [
+        'Travis Scott',
+        'Shabjdeed',
+        'Lil Uzi Vert',
+        'Don Toliver',
+        'DaBaby',
+      ],
+    },
+    {
+      moods: ['Sad', 'Depressed'],
+      artist: ['XXXTentacion', 'Juice WRLD', 'NF'],
+    },
+    {
+      moods: ['Calm'],
+      artist: [
+        'Post Malone',
+        'Bad Bunny',
+        'Don Toliver',
+        'Boogie wit da hoodie',
+      ],
+    },
+    {
+      moods: ['Romantic'],
+      artist: ['Lil Tjay', 'Juice WRLD', 'Ariana Grande', 'YNW Melly'],
+    },
+    {
+      moods: ['Loney'],
+      artist: ['NF', 'XXXTentaction', 'Juice WRLD'],
+    },
+    {
+      moods: ['Fearful'],
+      artist: ['Lucii'],
+    },
+  ];
+
+  const [mood, setMood] = useState({});
+
+  const randomMood = () => {
+    const len = allMoods.length;
+    setMood(Math.floor(Math.random() * len));
+    console.log(mood);
+  };
+
+  useEffect(() => {
+    randomMood();
+  }, []);
 
   return (
     <div className={Style.home__header}>
@@ -20,16 +70,35 @@ const Home = ({ mute, handleMuteChange }) => {
             </p>
           </Col>
           <Col md={4} sm={12} className="part2">
-            <p>Special thanks to:</p>
-            <button onClick={handleMuteChange}>click to change the prop</button>
-
-            <ul>
-              <li>Spotify API</li>
-              <li>styled-components</li>
-              <li>react-bootstrap</li>
-              <li>Jdenticon</li>
-              <li>Firebase</li>
-            </ul>
+            <div className={Style.card}>
+              <h1 className={Style.title}>
+                Feeling{' '}
+                {allMoods[mood] &&
+                  allMoods[mood].moods.map((rnmood) => (
+                    <span>
+                      {rnmood}
+                      {''}
+                      {allMoods[mood].moods.length ===
+                      allMoods[mood].moods.indexOf(rnmood) + 1
+                        ? null
+                        : ', '}
+                    </span>
+                  ))}{' '}
+                ?
+              </h1>
+              <div className={Style.artist}>
+                Go and listen to:
+                {allMoods[mood] &&
+                  allMoods[mood].artist.map((rnmood) => (
+                    <ul>
+                      <li>{rnmood}</li>
+                    </ul>
+                  ))}
+              </div>
+              <div className={Style.changeMood}>
+                <button onClick={randomMood}>into another mood?</button>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
