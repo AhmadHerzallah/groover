24b5c0d125b7a0f3861a09294d05a8ec370d0e49
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-04-27 12:48:00 +0300

    change: cursor to circle

---
+++ src/Components/Cursor.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [hidden, setHidden] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const [linkHovered, setLinkHovered] = useState(false);
+
+  useEffect(() => {
+    addEventListeners();
+    handleLinkHoverEvents();
+
+    return () => removeEventListeners();
+  }, []);
+
+  const addEventListeners = () => {
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseenter', onMouseEnter);
+    document.addEventListener('mouseleave', onMouseLeave);
+    document.addEventListener('mousedown', onMouseDown);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+  const removeEventListeners = () => {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseenter', onMouseEnter);
+    document.removeEventListener('mouseleave', onMouseLeave);
+    document.removeEventListener('mousedown', onMouseDown);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  const handleLinkHoverEvents = () => {
+    document.querySelectorAll('a').forEach((el) => {
+      el.addEventListener('mouseover', () => setLinkHovered(true));
+      el.addEventListener('mouseout', () => setLinkHovered(false));
+    });
+    document.querySelectorAll('button').forEach((el) => {
+      el.addEventListener('mouseover', () => setLinkHovered(true));
+      el.addEventListener('mouseout', () => setLinkHovered(false));
+    });
+  };
+
+  const onMouseLeave = () => {
+    setHidden(true);
+  };
+
+  const onMouseEnter = () => {
+    setHidden(false);
+  };
+
+  const onMouseDown = () => {
+    setClicked(true);
+  };
+
+  const onMouseUp = () => {
+    setClicked(false);
+  };
+
+  const onMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const cursorClasses = classNames('cursor', {
+    'cursor--hidden': hidden,
+    'cursor--clicked': clicked,
+    'cursor--link-hovered': linkHovered,
+  });
+  const isMobile = () => {
+    const ua = navigator.userAgent;
+    return /Android|Mobi/i.test(ua);
+  };
+  if (typeof navigator !== 'undefined' && isMobile()) return null;
+
+  return (
+    <div
+      className={cursorClasses}
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    />
+  );
+};
+
+export default Cursor;
