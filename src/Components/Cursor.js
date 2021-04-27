f3f8dc4232c9efdd9a2abd4faf11e4483bb6b8fa
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-04-27 14:30:42 +0300

    change: cursor style

--- src/Components/Cursor.js
+++ src/Components/Cursor.js
@@ -68,6 +68,8 @@ const Cursor = () => {
   });
   const isMobile = () => {
     const ua = navigator.userAgent;
+    console.log(navigator);
+    console.log(ua);
     return /Android|Mobi/i.test(ua);
   };
   if (typeof navigator !== 'undefined' && isMobile()) return null;
