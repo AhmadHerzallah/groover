53a16c8e7812dbb0dc2c8aca368838f5d88578cf
Hosny Al-khatib <onepunchman5421@gmail.com>
2021-05-02 20:16:35 +0300

    .

--- src/Components/Grinder.js
+++ src/Components/Grinder.js
@@ -5,10 +5,6 @@ import firebase from 'firebase';
 import { Container } from 'react-bootstrap';
 import Style from '../style/grinder.module.css';
 import TinderCard from 'react-tinder-card';
-<<<<<<< HEAD
-import { Database } from 'react-feather';
-=======
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
 
 const db = [
   {
@@ -44,11 +40,7 @@ const Grinder = () => {
       Array(db.length)
         .fill(0)
         .map((i) => React.createRef()),
-<<<<<<< HEAD
     []
-=======
-    [],
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
   );
 
   const obServer = () => {
@@ -65,18 +57,13 @@ const Grinder = () => {
     return (
       <Container>
         <p>
-<<<<<<< HEAD
           Sorry, you have to <a href='/login'>login</a> to see your profile.
-=======
-          Sorry, you have to <a href="/login">login</a> to see your profile.
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
         </p>
       </Container>
     );
   }
 
   const swiped = (direction, nameToDelete) => {
-<<<<<<< HEAD
     // if (direction === 'left') {
     //   var leftswaped = firebase.database.ref(user.id);
     //   console.log(leftswaped);
@@ -84,8 +71,6 @@ const Grinder = () => {
     //   leftswaped: leftswaped,
     // });
     // }
-=======
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
     console.log('removing: ' + nameToDelete);
     setLastDirection(direction);
     alreadyRemoved.push(nameToDelete);
@@ -93,98 +78,62 @@ const Grinder = () => {
 
   const outOfFrame = (name) => {
     console.log(name + ' left the screen!');
-    charactersState = charactersState.filter(
-<<<<<<< HEAD
-      (character) => character.name !== name
-=======
-      (character) => character.name !== name,
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
-    );
-    setCharacters(charactersState);
-  };
+    charactersState = charactersState.filter();
+    const swipe = (dir) => {
+      const cardsLeft = characters.filter(
+        (person) => !alreadyRemoved.includes(person.name)
+      );
+      if (cardsLeft.length) {
+        const toBeRemoved = cardsLeft[cardsLeft.length - 1].name; // Find the card object to be removed
+        const index = db.map((person) => person.name).indexOf(toBeRemoved); // Find the index of which to make the reference to
+        alreadyRemoved.push(toBeRemoved); // Make sure the next card gets removed next time if this card do not have time to exit the screen
+        childRefs[index].current.swipe(dir); // Swipe the card!
+      }
+    };
 
-  const swipe = (dir) => {
-    const cardsLeft = characters.filter(
-<<<<<<< HEAD
-      (person) => !alreadyRemoved.includes(person.name)
-=======
-      (person) => !alreadyRemoved.includes(person.name),
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
-    );
-    if (cardsLeft.length) {
-      const toBeRemoved = cardsLeft[cardsLeft.length - 1].name; // Find the card object to be removed
-      const index = db.map((person) => person.name).indexOf(toBeRemoved); // Find the index of which to make the reference to
-      alreadyRemoved.push(toBeRemoved); // Make sure the next card gets removed next time if this card do not have time to exit the screen
-      childRefs[index].current.swipe(dir); // Swipe the card!
-    }
-  };
-
-  return (
-    <>
-      <Container>
-        <div className={Style.grinder_main}>
-          <h1 className={`text-center`}>Grinder!</h1>
-          <div className={`${Style.grinder__container}`}>
-<<<<<<< HEAD
-            <div className='cardContainer'>
-              {characters.map((character, index) => (
-                <TinderCard
-                  ref={childRefs[index]}
-                  className='swipe'
-=======
-            <div className="cardContainer">
-              {characters.map((character, index) => (
-                <TinderCard
-                  ref={childRefs[index]}
-                  className="swipe"
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
-                  key={character.name}
-                  onSwipe={(dir) => swiped(dir, character.name)}
-                  onCardLeftScreen={() => outOfFrame(character.name)}
-                >
-                  <div
-                    style={{ backgroundImage: 'url(' + character.url + ')' }}
-<<<<<<< HEAD
-                    className='card'
-=======
-                    className="card"
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
+    return (
+      <>
+        <Container>
+          <div className={Style.grinder_main}>
+            <h1 className={`text-center`}>Grinder!</h1>
+            <div className={`${Style.grinder__container}`}>
+              <div className='cardContainer'>
+                {characters.map((character, index) => (
+                  <TinderCard
+                    ref={childRefs[index]}
+                    className='swipe'
+                    key={character.name}
+                    onSwipe={(dir) => swiped(dir, character.name)}
+                    onCardLeftScreen={() => outOfFrame(character.name)}
                   >
-                    <h3 style={{ color: '#000' }}>{character.name}</h3>
-                  </div>
-                </TinderCard>
-              ))}
-            </div>
-<<<<<<< HEAD
-            <div className='buttons'>
-=======
-            <div className="buttons">
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
-              <button onClick={() => swipe('left')}>Swipe left!</button>
-              <button onClick={() => swipe('up')}>Swipe up!</button>
+                    <div
+                      style={{ backgroundImage: 'url(' + character.url + ')' }}
+                      className='card'
+                    >
+                      <h3 style={{ color: '#000' }}>{character.name}</h3>
+                    </div>
+                  </TinderCard>
+                ))}
+              </div>
+              <div className='buttons'>
+                <button onClick={() => swipe('left')}>Swipe left!</button>
+                <button onClick={() => swipe('up')}>Swipe up!</button>
 
-              <button onClick={() => swipe('right')}>Swipe right!</button>
+                <button onClick={() => swipe('right')}>Swipe right!</button>
+              </div>
+              {lastDirection ? (
+                <h2 key={lastDirection} className='infoText'>
+                  Swiped {lastDirection}
+                </h2>
+              ) : (
+                <h2 className='infoText'>Start Swipin' !</h2>
+              )}
             </div>
-            {lastDirection ? (
-<<<<<<< HEAD
-              <h2 key={lastDirection} className='infoText'>
-                Swiped {lastDirection}
-              </h2>
-            ) : (
-              <h2 className='infoText'>Start Swipin' !</h2>
-=======
-              <h2 key={lastDirection} className="infoText">
-                Swiped {lastDirection}
-              </h2>
-            ) : (
-              <h2 className="infoText">Start Swipin' !</h2>
->>>>>>> f10caebfad7ff88e50878e79e4188c8e19a70221
-            )}
           </div>
-        </div>
-      </Container>
-    </>
-  );
+        </Container>
+      </>
+    );
+  };
 };
 
 export default Grinder;
