20c6bf578d797a90a4efe16055fb52b7b2ba45c5
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-05-24 16:32:07 +0300

    🥝 create: track card

---
+++ src/Components/SearchCards/TrackCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrackCard = () => {
+  return (
+    <div>
+      {/* code goes here */}
+      {/* and here */}
+    </div>
+  );
+};
+
+export default TrackCard;
