28d6d7e0d4c6fd68c8464821186e48d408bdf00f
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2021-06-20 05:35:18 +0300

    fun: add random avatar

--- src/Components/Pages/Profile.js
+++ src/Components/Pages/Profile.js
@@ -10,6 +10,7 @@ import fire from '../../fire';
 import Style from '../../style/profile.module.css';
 import { useAuth } from '../Authentication/Auth';
 import { useHistory } from 'react-router-dom';
+import Avatar, { genConfig, AvatarConfig } from 'react-nice-avatar';
 
 import firebase from 'firebase';
 import {
@@ -24,17 +25,17 @@ import Login from './Login';
 
 const Profile = () => {
   // Hooks
-
-  // const [user, setUser] = useState(null);
-  const history = useHistory();
-
-  const { currentUser, loading, name, email, logOut } = useAuth();
-  const db = firebase.database();
+  const { currentUser, loading, name, email, image, logOut } = useAuth();
+  const [avatarConfig, setAvatarConfig] = useState(null);
   const [data, setData] = useState({});
   const [phoroUrl, setPhotoUrl] = useState('');
-  const [image, setImage] = useState();
   const [uploadProgress, setUploadProgress] = useState(45);
   const uploadedImage = useRef('');
+  const history = useHistory();
+  let config = genConfig();
+  const db = firebase.database();
+
+  // const [user, setUser] = useState(null);
   let dataFromDatabase;
 
   // Storage
@@ -44,126 +45,34 @@ const Profile = () => {
 
   let storageRef = storage.ref();
 
-  // let imageRef = storageRef.child(`images/${user && user.uid}/profile.png`);
-  // let defaultImageRef = storageRef.child(`images/defaultDark.svg`);
-  /* Get Image from Storage */
-  // const ImageGetter = () => {
-  //   imageRef
-  //     .getDownloadURL()
-  //     .then((url) => {
-  //       setPhotoUrl(url);
-  //     })
-  //     .catch((error) => {
-  //       switch (error.code) {
-  //         case 'storage/object-not-found':
-  //           console.log('object-not-found');
-  //           break;
-  //         case 'storage/unauthorized':
-  //           console.log('unauthorized');
-  //           break;
-  //         case 'storage/canceled':
-  //           console.log('object-not-found');
-  //           break;
-  //         case 'storage/unknown':
-  //           console.log('object-not-found');
-  //           break;
-  //       }
-  //     });
-  // };
-  // Storage
-  // const users = db.ref('users');
-
-  // Get data from Realtime Database
-  // const getDataFromDatabase = () => {
-  //   console.log(users.child(user.uid)[email]);
-  //   users
-  //     .child(user.uid)
-  //     .get()
-  //     .then(function (snapshot) {
-  //       if (snapshot.exists()) {
-  //         setData(snapshot.val());
-  //         setTimeout(() => {
-  //           console.log('data', data);
-  //         }, 3000);
-  //         setPhotoUrl(snapshot.val().photo);
-  //       } else {
-  //         console.log('No data available');
-  //       }
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // };
-
-  // let username = data.username,
-  //   photo = data.photo,
-  //   email = data.email;
-  // const obServer = () => {
-  //   const authObserver = firebase.auth().onAuthStateChanged((user) => {
-  //     if (user) {
-  //       setUser(user);
-  //       setStatus('approved');
-  //     } else {
-  //       setUser(user);
-  //       setStatus('rejected');
-  //     }
-  //     console.log(`user:`, user);
-  //   });
-  //   user && user.uid && getDataFromDatabase();
-  //   return authObserver;
-  // };
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     obServer();
-  //   }, 1500);
-  // }, [user]);
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-
   const signOut = () => {
     logOut();
     history.push('/');
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   let type = uploadedImage.current.files[0].type;
-  //   let metadata = {
-  //     contentType: type,
-  //   };
-  //   let uploadTask = storageRef
-  //     .child('images/' + user.uid + '/profile')
-  //     .put(uploadedImage.current.files[0], metadata);
+  React.useLayoutEffect(() => {
+    let config = randomAvatar(name);
+    setAvatarConfig(config);
+  }, [name]);
 
-  //   uploadTask.on(
-  //     'state_changed',
-  //     (snapshot) => {
-  //       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-  //       setUploadProgress(progress);
-  //       console.log(progress);
-  //       switch (snapshot.state) {
-  //         case firebase.storage.TaskState.PAUSED:
-  //           console.log('Upload is paused');
-  //           break;
-  //         case firebase.storage.TaskState.RUNNING:
-  //           console.log('Upload is running');
-  //           break;
-  //       }
-  //     },
-  //     (err) => {
-  //       console.log('error: ', err);
-  //     },
-  //     (data) => {
-  //       uploadTask.snapshot.ref.getDownloadURL().then((url) => {
-  //         console.log(data);
-  //         users.child(user.uid).update({
-  //           photo: url,
-  //         });
-  //         setPhotoUrl(url);
-  //       });
-  //     },
-  //   );
-  // };
+  function randomAvatar(name) {
+    console.log(name);
+    const random = (items: string[]) =>
+      items[Math.floor(Math.random() * items.length)];
+    const calculatedSex = name === 'Bob Smith' ? 'man' : 'woman';
+    const earSize = ['small', 'big'];
+    const hairStyle = ['normal', 'thick', 'mohawk', 'womanLong', 'womanShort'];
+    const eyeStyle = ['circle', 'oval', 'smile'];
+    const glassesStyle = ['round', 'square', 'none'];
+    const noseStyle = ['short', 'long', 'round'];
+    const mouthStyle = ['laugh', 'smile', 'peace'];
+    const shirtStyle = ['hoody', 'short', 'polo'];
+
+    let randomAvatarConfig: AvatarConfig = {
+      sex: calculatedSex,
+    };
+    return genConfig(randomAvatarConfig);
+  }
 
   const hours = new Date().getHours();
   let greet = null;
@@ -183,14 +92,20 @@ const Profile = () => {
   } else if (currentUser) {
     return (
       <Container>
-        {phoroUrl ? (
+        {image ? (
           <img
             src={phoroUrl}
             className={Style.profile__image}
             alt={`${data && data.username}'s profile`}
           />
         ) : (
-          <img src={phoroUrl} alt='default profile' width='120' />
+          <Avatar
+            onClick={() => {
+              config = genConfig();
+            }}
+            className={Style.profile__image}
+            {...avatarConfig}
+          />
         )}
         <p className={Style.greetUser}>
           Good {greet && greet}, {name}
@@ -308,3 +223,119 @@ export default Profile;
 
 
     */
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   let type = uploadedImage.current.files[0].type;
+//   let metadata = {
+//     contentType: type,
+//   };
+//   let uploadTask = storageRef
+//     .child('images/' + user.uid + '/profile')
+//     .put(uploadedImage.current.files[0], metadata);
+
+//   uploadTask.on(
+//     'state_changed',
+//     (snapshot) => {
+//       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//       setUploadProgress(progress);
+//       console.log(progress);
+//       switch (snapshot.state) {
+//         case firebase.storage.TaskState.PAUSED:
+//           console.log('Upload is paused');
+//           break;
+//         case firebase.storage.TaskState.RUNNING:
+//           console.log('Upload is running');
+//           break;
+//       }
+//     },
+//     (err) => {
+//       console.log('error: ', err);
+//     },
+//     (data) => {
+//       uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+//         console.log(data);
+//         users.child(user.uid).update({
+//           photo: url,
+//         });
+//         setPhotoUrl(url);
+//       });
+//     },
+//   );
+// };
+
+// let imageRef = storageRef.child(`images/${user && user.uid}/profile.png`);
+// let defaultImageRef = storageRef.child(`images/defaultDark.svg`);
+/* Get Image from Storage */
+// const ImageGetter = () => {
+//   imageRef
+//     .getDownloadURL()
+//     .then((url) => {
+//       setPhotoUrl(url);
+//     })
+//     .catch((error) => {
+//       switch (error.code) {
+//         case 'storage/object-not-found':
+//           console.log('object-not-found');
+//           break;
+//         case 'storage/unauthorized':
+//           console.log('unauthorized');
+//           break;
+//         case 'storage/canceled':
+//           console.log('object-not-found');
+//           break;
+//         case 'storage/unknown':
+//           console.log('object-not-found');
+//           break;
+//       }
+//     });
+// };
+// Storage
+// const users = db.ref('users');
+
+// Get data from Realtime Database
+// const getDataFromDatabase = () => {
+//   console.log(users.child(user.uid)[email]);
+//   users
+//     .child(user.uid)
+//     .get()
+//     .then(function (snapshot) {
+//       if (snapshot.exists()) {
+//         setData(snapshot.val());
+//         setTimeout(() => {
+//           console.log('data', data);
+//         }, 3000);
+//         setPhotoUrl(snapshot.val().photo);
+//       } else {
+//         console.log('No data available');
+//       }
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//     });
+// };
+
+// let username = data.username,
+//   photo = data.photo,
+//   email = data.email;
+// const obServer = () => {
+//   const authObserver = firebase.auth().onAuthStateChanged((user) => {
+//     if (user) {
+//       setUser(user);
+//       setStatus('approved');
+//     } else {
+//       setUser(user);
+//       setStatus('rejected');
+//     }
+//     console.log(`user:`, user);
+//   });
+//   user && user.uid && getDataFromDatabase();
+//   return authObserver;
+// };
+// useEffect(() => {
+//   setTimeout(() => {
+//     obServer();
+//   }, 1500);
+// }, [user]);
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
